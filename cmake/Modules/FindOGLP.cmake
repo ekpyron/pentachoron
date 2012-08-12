273a2ad75f84d9ae9b2f0ede44799665d5dc95cf
Daniel Kirchner <daniel@ekpyron.org>
2012-08-12 22:17:51 +0200

    Fix FinOGLP

--- cmake/Modules/FindOGLP.cmake
+++ cmake/Modules/FindOGLP.cmake
@@ -10,10 +10,10 @@
 find_path (OGLP_INCLUDE_DIR oglp/oglp.h)
 find_library (OGLP_LIBRARY NAMES oglp)
 
-if (GLEW_INCLUDE_DIR)
-set (GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-else (GLEW_INCLUDE_DIR)
-set (GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-endif (GLEW_INCLUDE_DIR)
+if (OGLP_INCLUDE_DIR)
+set (OGLP_FOUND 1 CACHE STRING "Set to 1 if OGLP is found, 0 otherwise")
+else (OGLP_INCLUDE_DIR)
+set (OGLP_FOUND 0 CACHE STRING "Set to 1 if OGLP is found, 0 otherwise")
+endif (OGLP_INCLUDE_DIR)
 
-mark_as_advanced (GLEW_FOUND)
+mark_as_advanced (OGLP_FOUND)
